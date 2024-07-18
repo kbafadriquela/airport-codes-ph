c2b6948c91559bdcb9ada78c4a0b27e89e85c0f1
Krizia Bianca <krizia.dev@gmail.com>
2024-07-19 07:56:32 +0800

    Removed packages

--- src/ListDetails.js
+++
@@ -1,9 +0,0 @@
-export class AirportParams {
-    constructor(id, city, name, description, imageCredit) {
-        this.id = id;
-        this.city = city;
-        this.name = name;
-        this.description = description;
-        this.imageCredit = imageCredit;
-    }
-}
